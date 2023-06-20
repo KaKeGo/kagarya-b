f2044d01ab09f0769d699294bd41b041bff90a47
KaKeGo <funfcia@hotmail.com>
2023-06-20 17:48:19 +0200

    Create user manager for custom user

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #My App
+    'users',
+    
+    #Third apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set for user')
+        if not username:
+            raise ValueError('Username must be set for user')
+        
+        email = self.normalize_email(email)
+        user = self.model(
+                username=username, 
+                email=email, 
+                **extra_fields
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        return self.create_user(
+                username,
+                email,
+                password,
+                **extra_fields
+            )
