add301f68d9cde89988508a21050cafb88287604
KaKeGo <funfcia@hotmail.com>
2023-06-24 10:39:28 +0200

    git add database setting for test and change django_on_heroku to django heroku

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import django_on_heroku
+import django_heroku
 
 from pathlib import Path
 
@@ -87,12 +87,24 @@ WSGI_APPLICATION = 'kagarya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT'),
+        } 
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -168,4 +180,4 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
