7756f66ba969520fd220e90bfa06a907df23d12b
KaKeGo <funfcia@hotmail.com>
2023-06-24 15:42:32 +0200

    t

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -135,8 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -177,6 +175,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000'
     'http://127.0.0.1:8000',
  ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 django_heroku.settings(locals())
