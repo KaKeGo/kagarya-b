d653cd2e807eeef4394ffe143356af9b3e0f11e6
KaKeGo <funfcia@hotmail.com>
2023-06-24 11:03:52 +0200

    Add decouple to project to hide secrets

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -14,6 +14,7 @@ import os
 import django_heroku
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=w2cinrbyz=7@0g*rs&dz*ue5grx*dswasu%2+7mlsuikpab&z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
