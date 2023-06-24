c825aad78fa08b77f2a6a24a95c9b2390081ff93
KaKeGo <funfcia@hotmail.com>
2023-06-24 12:36:10 +0200

    Add templates navbar home and master design navbar and home in progress

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import ApiCategory
+
 # Create your views here.
 
 
 def homeView(request):
-    pass
+    categorys = ApiCategory.objects.all()
+    template = 'home/home.html'
+    context = {
+        'categorys': categorys,
+    }
+    return render(request, template, context)
