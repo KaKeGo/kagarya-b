04ba4314f7d8b438f2c5c80ced4553e928c3c8db
KaKeGo <funfcia@hotmail.com>
2023-06-24 19:46:54 +0200

    create model Todo plan, todo and task

--- kagarya/urls.py
+++ kagarya/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     
     path('', include('home.urls')),
     path('accounts/', include('users.urls')),
+    path('todo/', include('todo.urls')),
 ]
 
 if settings.DEBUG:
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TodoPlan(models.Model):
+    name = models.CharField(max_length=30)
+    todo = models.ManyToManyField('Todo', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Todo(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    category = models.ManyToManyField('Category', blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    name = models.CharField(max_length=40)
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    @staticmethod
+    def count_tasks(self):
+        return Task.objects.count()
+    
