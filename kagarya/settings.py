bb2a634a36602bef75d6fafd426a24d85c61e165
KaKeGo <funfcia@hotmail.com>
2023-07-03 19:50:32 +0200

    fix

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 from pathlib import Path
 from decouple import config
@@ -24,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'django-insecure-=w2cinrbyz=7@0g*rs&dz*ue5grx*dswasu%2+7mlsuikpab&z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
