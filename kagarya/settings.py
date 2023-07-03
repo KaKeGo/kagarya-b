7a801e03a9d311c40ac0a4a0365150a150dda41a
KaKeGo <funfcia@hotmail.com>
2023-07-03 18:10:49 +0200

    fix

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -91,20 +91,19 @@ WSGI_APPLICATION = 'kagarya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# if 'DATABASE_URL' in os.environ:
-import dj_database_url
-DATABASES = {
-    'default': 
-        dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-    
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {
+        'default': 
+            dj_database_url.config(conn_max_age=600, ssl_require=True)
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
