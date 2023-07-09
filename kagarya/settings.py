7d76384120281f5e46c0942bff86a96908f4e27d
KaKeGo <funfcia@hotmail.com>
2023-07-09 10:47:42 +0200

    Add description to Task

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -149,11 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STORAGES = {"staticfiles": {"BACKEND": "storages.backends.gcloud.GoogleCloudStorage"}}
-
-GS_BUCKET_NAME = 'kagarya'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
