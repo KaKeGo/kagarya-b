1d4a35c17528df411ee6327c0abe1b5437479187
KaKeGo <funfcia@hotmail.com>
2023-07-23 09:55:06 +0200

    change getcsrftoken view

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -173,9 +173,15 @@ if DEBUG is False:
             'rest_framework.renderers.JSONRenderer',
         ],
     }
+CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = False
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://www.kagarya.com',
+]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
