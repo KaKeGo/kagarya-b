701a8c7d0c94de9cade2a4e30601efdc00714102
KaKeGo <funfcia@hotmail.com>
2023-09-25 19:27:15 +0200

    Add urls for roles list and create

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 
 from . views import (
-    GetCSRFToken,
-    UserAuthCheckView,
-    ActivateAccountView,
-    
+    GetCSRFToken, UserAuthCheckView, ActivateAccountView,
     UserProfileDetailView,
-    UserRegisterView,
-    UserLoginView,
-    UserLogoutView,
-    
+    UserRegisterView, UserLoginView, UserLogoutView,
     ProfileListView,
-
+    RoleListView, RoleCreateView,
 )
 
 
@@ -21,12 +15,15 @@ app_name = 'users'
 urlpatterns = [
     path('csrftoken/', GetCSRFToken.as_view(), name='csrftoken'),
     path('authcheck/', UserAuthCheckView.as_view(), name='user_auth_check'),
-    
+    #Profile
     path('profile/list/', ProfileListView.as_view(), name='profile_list'),
     path('profile/create/', UserRegisterView.as_view(), name='profile_create'),
     path('activate/<str:uidb64>/<str:token>/', ActivateAccountView.as_view(), name='activate_account'),
     path('profile/login/', UserLoginView.as_view(), name='profile_login'),
     path('profile/logout/', UserLogoutView.as_view(), name='profile_logout'),
-    
+    #User Profile
     path('profile/<slug>/', UserProfileDetailView.as_view(), name='profile_detail'),
+    #Roles
+    path('roles/', RoleListView.as_view(), name='roles_list'),
+    path('roles/create/', RoleCreateView.as_view(), name='roles_create'),
 ]
